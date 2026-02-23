4926e902ff4fcc7b15a2ef61d247cbe676eaadad
odogu <odogu@nextgent.com>
2026-02-23 18:10:46 +0300

    build: trigger fresh deploy #2 (Dogukan)

--- frontend/dist/assets/zap-FDnPdRlH.js
+++
@@ -1,6 +0,0 @@
-import{R as o}from"./index-CrHf0WMG.js";/**
- * @license lucide-vue-next v0.344.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=o("ZapIcon",[["polygon",{points:"13 2 3 14 12 14 11 22 21 10 12 10 13 2",key:"45s27k"}]]);export{a as Z};
